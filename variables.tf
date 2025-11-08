567258bb92cb62d1960264b58b4cc2edc69d63ce
Mbaoma7 <mary.mbaoma@mtn.com>
2025-11-08 13:18:46 +0100

    add config for IAC

--- variables.tf
+++ variables.tf
@@ -81,7 +81,7 @@ variable "db_version" {
 variable "db_tier" {
   description = "Cloud SQL machine type"
   type        = string
-  default     = "db-perf-optimized-N-2"
+  default     = "db-perf-optimized-2-8"
 }
 
 variable "db_disk_size_gb" {
